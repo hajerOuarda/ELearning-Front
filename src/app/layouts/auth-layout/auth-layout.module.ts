f57e15ed34c69950dbbf23b9b351fa26de9d08d5
fadinouira <fadi.nouira1@gmail.com>
2021-04-06 20:34:52 +0200

    first commit

--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -5,8 +5,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthLayoutRoutes } from './auth-layout.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { LoginComponent } from '../../pages/login/login.component';
-import { RegisterComponent } from '../../pages/register/register.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -15,8 +13,7 @@ import { RegisterComponent } from '../../pages/register/register.component';
     // NgbModule
   ],
   declarations: [
-    LoginComponent,
-    RegisterComponent
+
   ]
 })
 export class AuthLayoutModule { }
